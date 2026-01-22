b69644b85c958d41b0da997d4d8271a3cb22a9c6
SwagMessiah02 <vmdemesquita@gmail.com>
2026-01-22 15:29:33 -0300

    refactor(app.js): Removendo trechos de código inúteis

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const { Amigo, Jogo, Emprestimo } = require('./models');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => res.redirect('/amigos'));
-app.use('/', routes);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
