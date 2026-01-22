bda0b9387725149ac1652c1eda26c28d35e4c63f
SwagMessiah02 <vmdemesquita@gmail.com>
2026-01-22 13:40:35 -0300

    refactor(routes.js): Organizando as rotas de acesso

---
+++ routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const AmigoController = require('./controllers/AmigoController');
+const JogosController = require('./controllers/JogosController');
+const EmprestimoController = require('./controllers/EmprestimoController');
+
+const router = express.Router();
+
+const amigoController = new AmigoController();
+const jogosController = new JogosController();
+const emprestimoController = new EmprestimoController();
+
+router.get('/amigos', amigoController.exibirAmigos);
+router.get('/amigos/novo', amigoController.exibirAdicionarAmigos);
+router.get('/amigos/editar/:id', amigoController.exibirEditarAmigo);
+router.post('/amigos/novo', amigoController.adicionarAmigos);
+router.post('/amigos/editar/:id', amigoController.editarAmigo);
+router.post('/amigos/excluir/:id', amigoController.excluirAmigos);
+
+router.get('/jogos', jogosController.exibirJogos);
+router.get('/jogos/novo', jogosController.exibirListaDeAmigos);
+router.get('/jogos/editar/:id', jogosController.exibirEditarJogo);
+router.post('/jogos/novo', jogosController.adicionarJogo);
+router.post('/jogos/editar/:id', jogosController.editarJogo);
+router.post('/jogos/excluir/:id', jogosController.excluirJogo);
+
+router.get('/emprestimos', emprestimoController.exibirEmprestimos);
+router.get('/emprestimos/novo', emprestimoController.exibirCriarEmprestimo);
+router.post('/emprestimos/novo', emprestimoController.criarEmprestimo);
+router.post('/emprestimos/excluir/:id', emprestimoController.excluirEmprestimo);
