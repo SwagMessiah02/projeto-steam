24d4290a3f1b08347ae968fea759ccebc28e2508
lyl-rich <richleoifpi@gmail.com>
2026-01-25 16:02:50 -0300

    Implementação de Listagem Completa dos Amigos em formato de PDF

--- controllers/AmigoController.js
+++ controllers/AmigoController.js
@@ -1,5 +1,6 @@
+const PDFDocument = require('pdfkit');
 const AmigoService = require('../services/AmigoService.js');
-const {Amigo} = require('../models');
+const { Amigo } = require('../models');
 
 class AmigoController {
     constructor () {
@@ -13,33 +14,33 @@ class AmigoController {
     }
 
     // /amigos/novo
-    exibirAdicionarAmigos = async (req, res) => res.render('amigos/novo');
+    exibirAdicionarAmigos = async (req, res) => {
+        res.render('amigos/novo');
+    }
 
     // /amigos/novo
     adicionarAmigos = async (req, res) => {
-        if(!req.body) {
-            console.log("Conteudo: " + req.body);
-        } else {
-            console.log(req.body);
-        }
-
         const { nome, email } = req.body;
-        await this.amigoService.createAmigo({nome, email})
+        await this.amigoService.createAmigo({ nome, email });
         res.redirect('/amigos');
     }
 
     // /amigos/editar/:id
     exibirEditarAmigo = async (req, res) => {
-        console.log(req.params.id);
         const amigo = await this.amigoService.getAmigoById(req.params.id);
-        if (!amigo) return res.status(404).send('Amigo não encontrado.');
+        if (!amigo) {
+            return res.status(404).send('Amigo não encontrado.');
+        }
         res.render('amigos/editar', { amigo });
     }
 
     // /amigos/editar/:id
     editarAmigo = async (req, res) => {
         const { nome, email } = req.body;
-        await this.amigoService.updateAmigo({ nome, email }, { where: { id: req.params.id } });
+        await this.amigoService.updateAmigo(
+            { nome, email },
+            { where: { id: req.params.id } }
+        );
         res.redirect('/amigos');
     }
 
@@ -49,11 +50,52 @@ class AmigoController {
         res.redirect('/amigos');
     }
 
-    exibirJson = async (req,res) => {
+    // /amigos/json
+    exibirJson = async (req, res) => {
         const data = await this.amigoService.getAmigosJson();
-
         res.status(200).json(data);
-    } 
+    }
+
+    // /amigos/pdf
+    gerarPdf = async (req, res) => {
+        const amigos = await this.amigoService.getAllAmigos();
+
+        const doc = new PDFDocument({ margin: 50 });
+
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader(
+            'Content-Disposition',
+            'inline; filename=relatorio-amigos.pdf'
+        );
+
+        doc.pipe(res);
+
+        // Título
+        doc
+            .fontSize(18)
+            .text('Relatório de Amigos', { align: 'center' });
+
+        doc.moveDown();
+
+        // Cabeçalho
+        doc.fontSize(12);
+        doc.text('ID', 50, doc.y, { continued: true });
+        doc.text('Nome', 100, doc.y, { continued: true });
+        doc.text('Email', 300, doc.y);
+
+        doc.moveDown(0.5);
+        doc.moveTo(50, doc.y).lineTo(550, doc.y).stroke();
+
+        // Dados
+        amigos.forEach(a => {
+            doc.moveDown(0.5);
+            doc.text(String(a.id), 50, doc.y, { continued: true });
+            doc.text(a.nome, 100, doc.y, { continued: true });
+            doc.text(a.email, 300, doc.y);
+        });
+
+        doc.end();
+    }
 }
 
 module.exports = AmigoController;
--- routes.js
+++ routes.js
@@ -9,18 +9,21 @@ const amigoController = new AmigoController();
 const jogosController = new JogosController();
 const emprestimoController = new EmprestimoController();
 
+// JSON
 router.get('/amigos/json', amigoController.exibirJson);
 router.get('/jogos/json', jogosController.exibirJson);
 router.get('/emprestimos/json', emprestimoController.exibirJson);
 
+// AMIGOS
 router.get('/amigos', amigoController.exibirAmigos);
+router.get('/amigos/pdf', amigoController.gerarPdf);
 router.get('/amigos/novo', amigoController.exibirAdicionarAmigos);
 router.get('/amigos/editar/:id', amigoController.exibirEditarAmigo);
-
 router.post('/amigos/novo', amigoController.adicionarAmigos);
 router.post('/amigos/editar/:id', amigoController.editarAmigo);
 router.post('/amigos/excluir/:id', amigoController.excluirAmigos);
 
+// JOGOS
 router.get('/jogos', jogosController.exibirJogos);
 router.get('/jogos/novo', jogosController.exibirListaDeAmigos);
 router.get('/jogos/editar/:id', jogosController.exibirEditarJogo);
@@ -28,6 +31,7 @@ router.post('/jogos/novo', jogosController.adicionarJogo);
 router.post('/jogos/editar/:id', jogosController.editarJogo);
 router.post('/jogos/excluir/:id', jogosController.excluirJogo);
 
+// EMPRESTIMOS
 router.get('/emprestimos', emprestimoController.exibirEmprestimos);
 router.get('/emprestimos/novo', emprestimoController.exibirCriarEmprestimo);
 router.post('/emprestimos/novo', emprestimoController.criarEmprestimo);
