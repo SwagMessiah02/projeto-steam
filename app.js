4fcc63a5b82c29666664f5e9625409f26624f247
SwagMessiah02 <vmdemesquita@gmail.com>
2026-01-25 09:27:45 -0300

    fix(app.js): Corrigindo erro nas requisições

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const routes = require('./routes.js');
 const path = require('path');
+
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
