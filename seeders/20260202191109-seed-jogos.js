90019bd54b9004df7676e7491341ea1554018a1f
SwagMessiah02 <vmdemesquita@gmail.com>
2026-02-02 17:19:47 -0300

    fix(seeders): Ajustando id dos amigos

--- seeders/20260202191109-seed-jogos.js
+++ seeders/20260202191109-seed-jogos.js
@@ -7,42 +7,42 @@ module.exports = {
       {
         titulo: "Killer 7",
         plataforma: "PC, Gamecube, Playstation 2",
-        amigoId: Number(21),
+        amigoId: Number(1),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         titulo: "Fallout: New Vegas",
         plataforma: "PC, Playstation 3, Xbox 360",
-        amigoId: Number(22),
+        amigoId: Number(2),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         titulo: "Fallout 76",
         plataforma: "PC, Playstation 4, Playstation 5, Xbox One, Xbox Series S, Xbox Series X",
-        amigoId: Number(23),
+        amigoId: Number(3),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         titulo: "Bloodborne",
         plataforma: "Playstation 4 & Playstation 5",
-        amigoId: Number(24),
+        amigoId: Number(4),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         titulo: "Demon's Souls",
         plataforma: "Playstation 3, Playstation 4, Playstation 5",
-        amigoId: Number(25),
+        amigoId: Number(5),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         titulo: "Persona 3 Reload",
         plataforma: "PC, Playstation 4, Playstation 5, Xbox Series S, Xbox Series X, Nintendo Switch 2",
-        amigoId: Number(26),
+        amigoId: Number(6),
         createdAt: new Date(),
         updatedAt: new Date()
       }
